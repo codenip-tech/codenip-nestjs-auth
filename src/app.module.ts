b2bfa1deeac5c94e50d1dc6c242aa7d60ab7fa1d
Juan González <juangonzalezmontes@gmail.com>
2021-10-10 01:34:51 +0200

    v2-database-dependencies
    - Add database dependencies
    - Update config to connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5000,
+      username: 'root',
+      password: 'codenip',
+      database: 'database',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
