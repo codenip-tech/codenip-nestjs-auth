5a18584bc9ed1db8c34656f6bf1406b333ef0f41
Juan González <juangonzalezmontes@gmail.com>
2021-10-12 22:48:01 +0200

    v9-register-user-part-3
    - Avoid duplicated emails

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -13,7 +13,7 @@ export class User {
   @Column({ length: 20 })
   name: string;
 
-  @Column({ length: 100 })
+  @Column({ length: 100, unique: true })
   email: string;
 
   @Column({ length: 100 })
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './user.entity';
@@ -7,6 +11,14 @@ export class UsersRepository extends Repository<User> {
   async createUser(registerUserDto: RegisterUserDto): Promise<void> {
     const { name, email, password } = registerUserDto;
     const user = this.create({ name, email, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (e) {
+      if (e.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('This email is already registered');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
