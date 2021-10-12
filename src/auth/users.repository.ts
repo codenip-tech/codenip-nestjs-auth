b88bb8a2f19cbcc7476b362ec4e833e674a9a617
Juan González <juangonzalezmontes@gmail.com>
2021-10-12 22:24:35 +0200

    v8-register-user-part-2
    - Add logic in UsersRepository

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -4,5 +4,9 @@ import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  async createUser(registerUserDto: RegisterUserDto): Promise<void> {}
+  async createUser(registerUserDto: RegisterUserDto): Promise<void> {
+    const { name, email, password } = registerUserDto;
+    const user = this.create({ name, email, password });
+    await this.save(user);
+  }
 }
