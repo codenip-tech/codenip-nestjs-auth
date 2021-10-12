14cd638e79ad9db6b23ab8903540d6f2fc9a4402
Juan González <juangonzalezmontes@gmail.com>
2021-10-12 22:55:55 +0200

    v10-register-user-part-4
    - Add validations in DTO

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,7 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class RegisterUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 20)
   name: string;
 
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
 }
