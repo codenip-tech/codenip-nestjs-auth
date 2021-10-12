118a963c0c1cb49e96bfa88ccac8ada1b110b614
Juan González <juangonzalezmontes@gmail.com>
2021-10-12 23:13:23 +0200

    v11-register-user-part-5
    - Encode password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(registerUserDto: RegisterUserDto): Promise<void> {
     const { name, email, password } = registerUserDto;
-    const user = this.create({ name, email, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ name, email, password: hashedPassword });
 
     try {
       await this.save(user);
